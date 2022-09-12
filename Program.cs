a482d0b26eea50d395f4ebf581cee5b914bb9a4c
Miconen <micropizza.mc@gmail.com>
2022-09-12 14:25:50 +0300

    Hello world

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Chess;
+
+namespace Chess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
