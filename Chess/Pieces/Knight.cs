740a1b496903654629a1b85af744fb667d1e1f7f
Niko <soder.n@gmail.com>
2022-09-19 12:33:19 +0300

    Add knight move

--- Chess/Pieces/Knight.cs
+++ Chess/Pieces/Knight.cs
@@ -1,5 +1,6 @@
 using System;
 using Chess;
+using Chess.Rules;
 
 namespace Chess.Pieces
 {
@@ -10,6 +11,25 @@ namespace Chess.Pieces
             this.color = color;
             this.nameShort = 'N';
             this.ghosting = true;
+        }
+
+        public override bool IsValidMove(Move move)
+        {
+            //Move move = new Move(fromTile, toTile, inputRank, inputFile, inputTargetRank, inputTargetFile);
+            /* addMove(this.move.inputRank + 1, y + 2, level);
+            addMove(x + 2, y + 1, level);
+            addMove(x + 2, y - 1, level);
+            addMove(x + 1, y - 2, level);
+            addMove(x - 1, y - 2, level);
+            addMove(x - 2, y - 1, level);
+            addMove(x - 2, y + 1, level);
+            addMove(x - 1, y + 2, level);
+            return true; */
+            int min = Math.Min(move.fromRank - move.toRank, move.fromFile - move.toFile);
+            int max = Math.Max(move.fromRank - move.toRank, move.fromFile - move.toFile);
+
+            if (Math.Abs(min) == 1 && Math.Abs(max) == 2) return true;
+            return false;
         }
     }
 }
